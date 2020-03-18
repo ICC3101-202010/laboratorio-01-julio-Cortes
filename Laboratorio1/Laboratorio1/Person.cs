6294bd6fbbb91bf47c825fe5c163cbbb2cee1271
Julio_Cortes <jicortes@miuandes.cl>
2020-03-18 12:54:49 -0300

    ClasePerson

---
+++ Laboratorio1/Laboratorio1/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1
+{
+    public class Person
+    {
+        public string Name;
+        public string LastName;
+        public void Cachipun()
+        {
+            var random = new Random ();
+            Console.WriteLine (random.Next(0, 2));
+        }
+
+    }
+}
+    
---
+++ Laboratorio1/Laboratorio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person();
+            person1.Name = "Juan";
+            person1.LastName = "Perez";
+            person1.Cachipun();
+            var person2 = new Person();
+            person2.Name = "Pedro";
+            person2.LastName = "Rodriguez";
+            person2.Cachipun();
+
+        }
+    }
+}
